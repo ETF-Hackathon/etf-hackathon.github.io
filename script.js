91b3cb54a0b20b205af69e2b7a1741380cff320d
erictheeric <ehu5591@bths.edu>
2020-06-29 15:02:59 -0400

    what is wrong with git commit -a

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', _ => {
     window.onscroll = _ => {
         if (document.documentElement.scrollTop > 0.1 || document.body.scrollTop > 80) {
             document.querySelector('nav').style.height = '10vh'
