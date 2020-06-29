5bd6a307c502f2a8c2b317a94a89546aa9282a0d
erictheeric <ehu5591@bths.edu>
2020-06-29 14:55:35 -0400

    lmk if this looks ugly, i changed up some colors

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     window.onscroll = _ => {
         if (document.documentElement.scrollTop > 0.1 || document.body.scrollTop > 80) {
             document.querySelector('nav').style.height = '10vh'
+            document.querySelector('nav').style.backgroundColor = '#cecece'
         }
         else {
             document.querySelector('nav').style.height = '20vh'
+            document.querySelector('nav').style.border = 'none'
+            document.querySelector('nav').style.backgroundColor = '#e2e2e2'
         }
     }
 })
