cd62d5d327ae94ec73e68a506a1faf069e204c5c
Hao Teng <haoteng10@gmail.com>
2020-07-03 00:18:58 -0400

    Fix JavaScript for logo

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', _ => {
         if (document.documentElement.scrollTop > 1 || document.body.scrollTop > 1) {
             document.querySelector('nav').style.height = '10vh'
             document.querySelector('nav').style.backgroundColor = '#cecece'
-            document.querySelector('#logoPic').style.height = '60px'
-            document.querySelector('#logoPic').style.width = '60px'
+            document.querySelector('#logo-pic').style.height = '60px'
+            document.querySelector('#logo-pic').style.width = '60px'
             document.querySelector('#logoText').style.fontSize = '17px'
 
         }
@@ -12,14 +12,14 @@ document.addEventListener('DOMContentLoaded', _ => {
             document.querySelector('nav').style.height = '20vh'
             document.querySelector('nav').style.border = 'none'
             document.querySelector('nav').style.backgroundColor = '#e2e2e2'
-            document.querySelector('#logoPic').style.height = '75px'
-            document.querySelector('#logoPic').style.width = '75px'
+            document.querySelector('#logo-pic').style.height = '75px'
+            document.querySelector('#logo-pic').style.width = '75px'
             document.querySelector('#logoText').style.fontSize = '20px'
         }
     }
 })
 
-// #logoPic {
+// #logo-pic {
 //     height: 60px;
 //     width: 60px;
 // }
